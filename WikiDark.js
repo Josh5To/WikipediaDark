f583d35202beb3b20370d9cfbf916c56e10f6323
josh5to <josh@5to.tech>
2021-09-20 12:08:39 -0400

    td and tr background-color are now only changed if they have no color set. As to not disturb color coated tables.

--- WikiDark.js
+++ WikiDark.js
@@ -1,6 +1,45 @@
-document.body.style.backgroundColor = "#121212 !important";
-document.body.getElementById("mw-page-base").style.setProperty('background-color', '#121212', 'important')
-divs = document.body.getElementsByTagName(div)
-for (div in divs) {
-    div.style.setProperty('background-color', '#121212', 'important')
+var tds = document.body.getElementsByTagName("td")
+for (var i = 0; i < tds.length; i++) {
+    //tds[i].style.backgroundColor =  "#121212"
+    let rgb = tds[i].style.backgroundColor
+    //If no background color is given, make it match the body background
+    if(!!rgb) {
+        tds[i].style.backgroundColor =  "#121212"
+    }
+    console.log(rgb)
+}
+var trs = document.body.getElementsByTagName("tr")
+for (var i = 0; i < trs.length; i++) {
+    //trs[i].style.backgroundColor =  "#121212"
+    let rgb = trs[i].style.backgroundColor
+    //If no background color is given, make it match the body background
+    if(!!rgb) {
+        trs[i].style.backgroundColor =  "#121212"
+    }
+    console.log(rgb)
+}
+
+document.body.style.margin = "20px !important"
+
+
+function rgbToHEX(r, g, b, a) {
+    let rString = r.toString(16)
+    let gString = g.toString(16)
+    let bString = b.toString(16)
+    let aString = Math.round(a * 255).toString(16)
+
+    if (rString.length ==1) {
+        rString = "0" + rString
+    }
+    if (gString.length ==1) {
+        gString = "0" + gString
+    }
+    if (bString.length ==1) {
+        bString = "0" + bString
+    }
+    if (aString.length ==1) {
+        aString = "0" + aString
+    }
+
+    return "#" + rString + gString + bString + aString
 }
